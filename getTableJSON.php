adfc8be28fe8a06056a992c88956b7904cd3b5c4
Chris Lane <clane359@gmail.com>
2019-03-23 19:28:58 -0700

    saving

--- getTableJSON.php
+++ getTableJSON.php
@@ -8,23 +8,23 @@ $dbname = 'clane_BDS';
 $table = 'gigs';
 
 //Create database connection
-  $dblink = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$dblink = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 //Check connection was successful
-  if ($dblink->connect_errno) {
-     printf("Failed to connect to database");
-     exit();
-  }
+if ($dblink->connect_errno) {
+   printf("Failed to connect to database");
+   exit();
+}
 
-  $result = $dblink->query("SELECT * FROM gigs");
+$result = $dblink->query("SELECT * FROM $table");
 
 //Initialize array variable
-  $dbdata = array();
+$dbdata = array();
 
 //Fetch into associative array
-  while ( $row = $result->fetch_assoc())  {
+while ( $row = $result->fetch_assoc())  {
 	$dbdata[]=$row;
-  }
+}
 
 //Print array in JSON format
  echo json_encode($dbdata);
