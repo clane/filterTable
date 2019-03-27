46137fe33f4be13a148171dc7a31022acdd73b17
Chris Lane <clane359@gmail.com>
2019-03-26 21:40:16 -0700

    added some error prevention

--- insert.php
+++ insert.php
@@ -7,9 +7,14 @@ $priority =  $_POST['priority'];
 
 include 'dbVars.php';
 $query =  "INSERT INTO $table (`id`, `description`, `title`, `status`, `priority`) VALUES (NULL, '$description', '$title', '$status', $priority)";
-$result = $dblink->query($query);
 
-if($result){
+if($description && $title && $status && $priority){
+	$result = $dblink->query($query);
+} else {
+	$result = 0; 
+}
+
+if($result == 1){
 	echo '<p style="font-size:3rem;margin:20px;">' . 'Success' . '</p>';
 } else {
 	echo '<p style="font-size:3rem;color:red;margin:20px">Error</p>';
