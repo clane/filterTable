191d9cd740f6df3d1a4905bc27444d42911b24a0
Chris Lane <clane359@gmail.com>
2019-03-29 14:48:42 -0700

    now printing num results

--- manageRecord.php
+++ manageRecord.php
@@ -10,16 +10,26 @@ $query =  "SELECT * FROM $table";
 /* Select queries return a resultset */
 if ($result = mysqli_query($dblink, $query)) {
 
-    printf("Select returned %d rows.\n", mysqli_num_rows($result));
+    echo '<p>Query returned '. mysqli_num_rows($result) . ' rows</p>';
 
 	$fieldValues = array();
+	$fieldNames = array();
 
 	while ($row = mysqli_fetch_row($result)) {
+
+        /* Get field information for all columns */
+        $fieldInfo = $result->fetch_fields();
+
+        foreach ($fieldInfo as $fieldInfoValues) {
+			array_push($fieldNames,$fieldInfoValues->name);
+        }
+
 		foreach ($row as $value) {
 			array_push($fieldValues,$value);
 		}
 
 		for ($i = 0; $i < count($fieldValues); $i++) {
+		  print $fieldNames[$i];
 		  print $fieldValues[$i];
 		}
 
