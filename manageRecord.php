0935117f09e97f028dbe174a9ad95f4551cb6c72
Chris Lane <clane359@gmail.com>
2019-03-29 13:30:13 -0700

    starting to print out row data

--- manageRecord.php
+++ manageRecord.php
@@ -7,19 +7,18 @@ include 'top.php';
 
 $query =  "SELECT * FROM $table";
 
-
 /* Select queries return a resultset */
 if ($result = mysqli_query($dblink, $query)) {
+
     printf("Select returned %d rows.\n", mysqli_num_rows($result));
 
 	while ($row = mysqli_fetch_row($result)) {
-		printf("%s\n", $row[0]);
-		printf("%s\n", $row[1]);
-		printf("%s\n", $row[2]);
-		printf("%s\n", $row[3]);
-		printf("%s\n", $row[4]);
-     }
 
+		foreach ($row as $key => $value) {
+			echo "{$key} => {$value} ";
+		}
+
+	}
     /* free result set */
     mysqli_free_result($result);
 
