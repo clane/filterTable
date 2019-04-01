09421e02160bf8bca340dc4a0272b7c2b8915265
Chris Lane <clane359@gmail.com>
2019-03-31 17:37:02 -0700

    updates now working

--- update.php
+++ update.php
@@ -14,8 +14,15 @@ foreach ($_POST as $param_name => $param_val) {
 	if($column == 'id'){
 		$id = $value;
 	} else {
-		$query = "UPDATE $table SET $column = $value WHERE id = $id";
+		$query = "UPDATE $table SET $column = '$value' WHERE id = $id";
 		echo '<div>' . $query . '</div>';
+
+		if ($result = mysqli_query($dblink, $query)) {
+			echo '<p>Query succeeded!</p>';
+		} else {
+			echo '<p>Query failed!</p>';
+		}
+
 	} 
 }
 
