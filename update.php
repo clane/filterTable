b15d4e7dcdeaf34f18354cb7f5d087b32ed4249f
Chris Lane <clane359@gmail.com>
2019-03-31 20:41:58 -0700

    back button on update.php now works

--- update.php
+++ update.php
@@ -6,25 +6,22 @@ include 'top.php';
 
 foreach ($_GET as $param_name => $param_val) {
 
-		echo "Param: $param_name; Value: $param_val<br />\n";
-
 		$column = $param_name;
 		$value = $param_val;
 
 	if($column == 'id'){
 		$id = $value;
+		echo '<a href="./manageRecord.php?id=' . $id . '">Back to form</a>'; 
 	} else {
 		$query = "UPDATE $table SET $column = '$value' WHERE id = $id";
-		echo '<div>' . $query . '</div>';
 
-		if ($result = mysqli_query($dblink, $query)) {
-			echo '<p>Query succeeded!</p>';
-		} else {
+		if (!$result = mysqli_query($dblink, $query)) {
 			echo '<p>Query failed!</p>';
 		}
 
 	} 
 }
+
 
 include 'bottom.php';
 
