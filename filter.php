20e4fa0d48e48709ee80669ac49bca36b4b0b13d
Chris Lane <clane359@gmail.com>
2019-04-04 19:16:26 -0700

    now capturing GET params from the filter form

--- filter.php
+++ filter.php
@@ -1,31 +1,9 @@
 <?php
 
 	include 'dbVars.php';
-	$columns = '*';
 
-	$colToFilter = $_GET['colToFilter'];
-	$searchString = $_GET['searchString'];
+	print_r($_GET);
 
-    $regEx = "'" . aa . "'";
-
-	$colToFilter = 'description';
-
-	$query = "SELECT $columns FROM $table WHERE $colToFilter REGEXP $regEx";
-   
-	echo $query;
- 
-	$result = $dblink->query($query);
-
-	//Initialize array variable
-	$dbdata = array();
-
-	//Fetch into associative array
-	while ( $row = $result->fetch_assoc())  {
-		$dbdata[]=$row;
-	}
-
-	//Print array in JSON format
-	echo json_encode($dbdata);
 
 ?>
 
