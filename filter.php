902340c976d7ec2798654810faf359337bb00cd4
Chris Lane <clane359@gmail.com>
2019-04-05 11:33:11 -0700

    setting default checked state for sorting radio buttons

--- filter.php
+++ filter.php
@@ -1,10 +1,30 @@
 <?php
 
+include 'dbVars.php';
 include 'top.php';
 
+$title = "";
+$columns = '*';
+$select = "SELECT * FROM $table";
+$where = "WHERE";
+$query = $select + $where;
+
+$colsToSearch = [];
+$searchStrings = [];
+
 foreach($_GET as $key => $value){
-	echo $key . " : " . $value . "<br />";
+	if($key && $value) {
+		array_push($colsToSearch, $key);
+		array_push($searchStrings, $value);
+	} else {
+		echo "<p class=\"error\">Error: There is an error with the entry for $key</p>";
+	}  
 }
+
+for ($x = 0; $x < sizeOf(colsToSearch); $x++) {
+    echo "The number is: $x <br>";
+} 
+
 
 include 'bottom.php';
 
