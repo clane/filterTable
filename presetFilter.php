ac7f5357ab70905b69650ab553ebdcbf3588d28f
Chris Lane <clane359@gmail.com>
2019-04-10 11:34:29 -0700

    saving

--- presetFilter.php
+++ presetFilter.php
@@ -8,23 +8,10 @@ $query = "SHOW COLUMNS FROM $table";
 $where = 'WHERE ';
 $result = $dblink->query($query);
 $dbdata = array();
-
-$sortColumn = $_GET['filter_sort_column'];
-$sortDirection = $_GET['filter_sort_direction'];
+$sortColumn = $_GET['sortCol'];
+$sortDirection = $_GET['sortDir'];
+$searchTerms = $_GET['searchTerms'];
 $orderBy = "ORDER BY $sortColumn $sortDirection";
-
-//<input type="checkbox" name="presetSearchTerm_0" value="test">
-
-//get the search strings
-foreach($_GET as $key => $value){
-    if($key && $value) {
-        if($key != 'filter_sort_column' && $key != 'filter_sort_direction'){
-            array_push($colsToSearch, $key);
-            array_push($searchStrings, $value);
-        }
-    }
-}
-
 
 //get the column names
 while ($row = $result->fetch_assoc())  {
@@ -35,13 +22,15 @@ while ($row = $result->fetch_assoc())  {
 	}
 }
 
-//create the where clause
-for($i = 0; $i < sizeOf($colsToSearch); $i++){
-	$where .= "$colsToSearch[$i] ";
-	$where .= "LIKE '%" . $searchTerm . "%'"; 
-	if($i < (sizeOf($colsToSearch) - 1))  {
-		$where .= " OR ";
-	} 
+for($i = 0; $i < sizeOf($searchTerms); $i++){
+	//create the where clause
+	for($j = 0; $j < sizeOf($colsToSearch); $j++){
+		$where .= "$colsToSearch[$j] ";
+		$where .= "LIKE '%" . $searchTerms[$i] . "%'"; 
+		if($j < (sizeOf($colsToSearch) - 1))  {
+			$where .= " OR ";
+		} 
+	}
 }
 
 $selectQuery = "SELECT * FROM $table $where $orderBy";
