dd151cae1b7e454b5a20bc821e629f414f85803d
Chris Lane <clane359@gmail.com>
2019-04-10 00:46:52 -0700

    saving

--- presetFilter.php
+++ presetFilter.php
@@ -2,30 +2,42 @@
 
 include 'dbVars.php';
 
-$columns = '*';
-$select = "SELECT * FROM $table";
-
 $colsToSearch = [];
-$searchStrings = [];
-
+$searchTerm = 'aaaaaa';
 $query = "SHOW COLUMNS FROM $table";
-
+$where = 'WHERE ';
 $result = $dblink->query($query);
-
 $dbdata = array();
+
 while ($row = $result->fetch_assoc())  {
 	$dbdata[]=$row;
 	$fieldname = $row['Field'];
 	if( $fieldname !== 'id') {
 		array_push($colsToSearch, $fieldname); 
 	}
-
 }
 
 for($i = 0; $i < sizeOf($colsToSearch); $i++){
-	echo $colsToSearch[$i]; 
+	$where .= "$colsToSearch[$i]=\"$searchTerm\"";
+	if($i < (sizeOf($colsToSearch) - 1))  {
+		$where .= " OR ";
+	} 
 }
 
+$selectQuery = "SELECT * FROM $table $where";
+
+$selectResult = $dblink->query($selectQuery);
+
+//Initialize array variable
+$dbdata = array();
+
+//Fetch into associative array
+while ( $row = $selectResult->fetch_assoc())  {
+		$dbdata[]=$row;
+}
+
+//Print array in JSON format
+ echo json_encode($dbdata);
 
 
 ?>
