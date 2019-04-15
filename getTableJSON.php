e0e9f4239cb95b7c23f41a37dd390034809040c2
Chris Lane <clane359@gmail.com>
2019-04-15 16:42:05 -0700

    added back file to get fields for entry form

---
+++ getTableJSON.php
@@ -0,0 +1,20 @@
+
+<?php
+	include 'dbVars.php';
+	$columns = '*';
+	$orderBy = 'id DESC'; 
+	if ($_GET['orderby']) {
+		$orderBy = $_GET['orderby']; 
+	}
+	$query = "SELECT $columns FROM $table ORDER BY $orderBy";
+	$result = $dblink->query($query);
+	//Initialize array variable
+	$dbdata = array();
+	//Fetch into associative array
+	while ( $row = $result->fetch_assoc())  {
+		$dbdata[]=$row;
+	}
+	//Print array in JSON format
+	 echo json_encode($dbdata);
+?>
+
