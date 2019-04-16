1d5a8b4e937272795905b79b0961e806f95bcf4b
Chris Lane <clane359@gmail.com>
2019-04-16 11:07:35 -0700

    saving

--- update.php
+++ update.php
@@ -1,9 +1,9 @@
 
 <?php
-$pageTitle = "Record Update Results";
+$pageTitle = "Search Term Record Update Results";
 include 'dbVars.php';
 include 'top.php';
-
+ 
 foreach ($_GET as $param_name => $param_val) {
 
 		$column = $param_name;
@@ -12,9 +12,9 @@ foreach ($_GET as $param_name => $param_val) {
 	if($column == 'id'){
 		$id = $value;
 		echo '<p>Update succeeded!</p>';
-		echo '<a href="./manageRecord.php?id=' . $id . '">Back to form</a>'; 
+		echo '<a href="./manageSearchTermRecord.php?id=' . $id . '">Back to form</a>'; 
 	} else {
-		$query = "UPDATE $table SET $column = '$value' WHERE id = $id";
+		$query = "UPDATE $presetSearchTermsTable SET $column = '$value' WHERE id = $id";
 
 		if (!$result = mysqli_query($dblink, $query)) {
 			echo '<p class="error">Query failed!</p>';
